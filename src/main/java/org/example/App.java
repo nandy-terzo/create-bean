8ddab03866a560124711a906e333c5948409d898
nandha kumar <nandhakumar@nandha.local>
2022-06-13 15:02:19 +0530

    handled exception using name of func

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         var context= new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Parrot p=context.getBean(Parrot.class);
+        Parrot p=context.getBean("parrot2",Parrot.class);
         System.out.println(p.getName());
     }
 }
